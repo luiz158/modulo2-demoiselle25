2d6816ccfc962c418b73d2c0e810741f5f74eb31
Wilson Barbosa <wilson.barbosa@gmail.com>
2015-09-16 18:15:43 -0300

    M2C2  final

--- deploy-cadastro.sh
+++ deploy-cadastro.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-cp /home/09255340808/trab/workspace/cadastro/target/cadastro-0.0.1-SNAPSHOT.war /opt/demoiselle/server/jboss-eap-6.3/standalone/deployments/cadastro.war 
+cp /home/09255340808/trab/workspace/cadastro/target/cadastro-0.0.1-SNAPSHOT.war\
+ /opt/demoiselle/server/jboss-eap-6.3/standalone/deployments/cadastro.war 
 
 exit 0
